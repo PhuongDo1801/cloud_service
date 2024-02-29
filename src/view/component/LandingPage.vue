3599fcae82ee78c73604a521f3a3627c08f6ffb5
PhuongDo1801 <92222210+PhuongDo1801@users.noreply.github.com>
2024-02-29 23:08:09 +0700

    change landing page

---
+++ src/view/component/LandingPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="header">
+            <div>Giới thiệu</div>
+            <div>Hướng dẫn</div>
+            <div>Login</div>
+        </div>
+        <div class="main-intro">
+
+        </div>
+        <div class="footer">
+
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: "LandingPage",
+        data(){
+            return {
+                
+            }
+        },
+        methods: {
+            
+        },
+    }
+</script>
+<style>
+</style>
